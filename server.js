4199aaecc84d86ee490bdafeef88581f0b46c6c9
WhatsMyName <128970881+whatsmyname12@users.noreply.github.com>
2023-12-30 19:59:10 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,3 +1,41 @@
+// server.js
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        serveHTML('login1.html', res);
+    } else if (req.url === '/login2') {
+        serveHTML('login2.html', res);
+    } else {
+        // Handle other requests (if any)
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
+});
+
+function serveHTML(fileName, res) {
+    const filePath = path.join(__dirname, fileName);
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Internal Server Error');
+            return;
+        }
+
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+    });
+}
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}/`);
+});
+
+
 app.use(cors());
 app.get("/", (req, res) => {
 	res.setHeader("Access-Control-Allow-Credentials","true");
